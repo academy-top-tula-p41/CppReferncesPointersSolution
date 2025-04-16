66f6c643eda781e7cdd64144a824606949ef1789
maxefimov <efimov.top.academy@gmail.com>
2025-04-16 21:13:37 +0300

    Добавьте файлы проекта.

---
+++ CppReferncesProject/CppReferncesProject.cpp
@@ -0,0 +1,27 @@
+﻿// CppReferncesProject.cpp 
+//
+
+#include <iostream>
+
+void func(int& n)
+{
+    n = 456;
+}
+
+void swap(int& x, int& y)
+{
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+int main()
+{
+    int a = 20;
+    int b = 30;
+    std::cout << a << " " << b << "\n";
+    
+    swap(a, b);
+    std::cout << a << " " << b << "\n";
+
+}
